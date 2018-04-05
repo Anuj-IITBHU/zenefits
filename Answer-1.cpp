9e9abdc71c4af1f9ee1cbacdce03d26b3a02d97b
anujbhu13 <31030384+anujbhu13@users.noreply.github.com>
2018-04-05 19:41:01 +0530

    Add files via upload

---
+++ Answer-1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+inorder (string s, int i)
+{
+    if(s[i])
+}
+
+int main() {
+	int t;
+	cin >> t;
+	string s[t];
+	for(int i=0;i<t;i++)
+	{
+	    cin >> s[i];
+	}
+	for(int i=0;i<t;i++)
+	    inorder(s[i], 0);
+    return 0;
+}
